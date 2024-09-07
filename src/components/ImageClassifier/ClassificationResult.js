1eb90c137ab13d21df8812919a33770f71afd9ed
Ahamed Reezan Saleem <it21042324@my.sliit.lk>
2024-09-07 22:53:07 +0530

    New: Changed UI COlor

--- src/components/ImageClassifier/ClassificationResult.js
+++ src/components/ImageClassifier/ClassificationResult.js
@@ -5,6 +5,13 @@ export const ClassificationResult = ({
   isClassified,
   classificationResult,
 }) => {
+  // Find the highest probability value
+  const highestProbabilityIndex = classificationResult
+    ? classificationResult.probabilities.indexOf(
+        Math.max(...classificationResult.probabilities)
+      )
+    : -1;
+
   return (
     <div>
       {isLoading && <div className={styles.loadingText}>Classifying...</div>}
@@ -12,22 +19,40 @@ export const ClassificationResult = ({
       {isClassified && classificationResult && (
         <div className={styles.resultContainer}>
           {classificationResult.result === "Normal" ? (
-            <>
-              <div className={styles.resultText}>
-                Classification Result: {classificationResult.result}
+            <div className={styles.normalContainer}>
+              <div className={styles.result}>
+                Classification Result:{" "}
+                <span className={styles.resultText}>{"No Acne"}</span>
               </div>
-              <div className={styles.confidenceText}>
-                Confidence: {(classificationResult.confidence * 100).toFixed(2)}
-                %
+
+              <div className={styles.confidence}>
+                Confidence Score:
+                <span className={styles.resultText}>
+                  {" "}
+                  {(classificationResult.confidence * 100).toFixed(2)}%
+                </span>
               </div>
-            </>
+            </div>
           ) : (
             <div className={styles.acneTypesContainer}>
               <div className={styles.acneTypeGrid}>
                 {classificationResult.acneTypes.map((type, index) => (
-                  <div className={styles.acneTypeItem} key={index}>
+                  <div
+                    className={`${styles.acneTypeItem} ${
+                      index === highestProbabilityIndex
+                        ? styles.highestProbability
+                        : ""
+                    }`}
+                    key={index}
+                  >
                     <span className={styles.acneType}>{type}</span>
-                    <span className={styles.acneProbability}>
+                    <span
+                      className={
+                        index === highestProbabilityIndex
+                          ? styles.acneProbabilityHighest
+                          : styles.acneProbability
+                      }
+                    >
                       {(
                         classificationResult.probabilities[index] * 100
                       ).toFixed(2)}
