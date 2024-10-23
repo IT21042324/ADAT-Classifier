e55f87c298641c357c27cffe31791c09fa6f1ce9
Ahamed Reezan Saleem <it21042324@my.sliit.lk>
2024-10-24 01:05:50 +0530

    New: Added different style to low quality image swal

--- src/components/useHook/useSwalMessage.js
+++ src/components/useHook/useSwalMessage.js
@@ -5,20 +5,34 @@ export const useSwalMessage = () => {
   return {
     lowQualityImageError: (extractedFaceImage, quality_score) => {
       Swal.fire({
-        icon: "error",
-        title: "Low Image Quality",
+        title: "Poor Image Quality Detected",
         html: `
-                  <p>Please upload a clearer image!</p>
-                  <img src="${extractedFaceImage}" alt="Low Quality Image" class="${styles.uploadedImage}" style="max-width: 100%; height: auto;"/>
-                `,
+          <p>Please upload a clearer image!</p>
+          <img id="uploadedImage" src="${extractedFaceImage}" alt="Low Quality Image" class="${styles.uploadedImage}" style="max-width: 100%; height: auto;"/>
+        `,
         footer: `
-                  <p>The uploaded image has a quality score of <strong>${quality_score.toFixed(
-                    2
-                  )}</strong>.</p>
-                  <p>The minimum required quality score is <strong>0.4</strong> for accurate classification.</p>
-                `,
+          <p><span class="${
+            styles.qualityScore
+          }">The image quality score is <strong>${quality_score.toFixed(
+          2
+        )}</strong></span>, which is below the required <strong>0.4</strong> for accurate classification.</p>
+        `,
+        confirmButtonText: "Ok",
+        customClass: {
+          confirmButton: styles.confirmButton,
+        },
+        buttonsStyling: false,
+        didOpen: () => {
+          // Set the width of the confirm button to match the image width
+          const image = document.querySelector(`.${styles.uploadedImage}`);
+          const button = document.querySelector(`.${styles.confirmButton}`);
+          if (image && button) {
+            button.style.width = `${image.clientWidth}px`;
+          }
+        },
       });
     },
+
     imageSizeTooLargeError: () => {
       Swal.fire({
         icon: "error",
